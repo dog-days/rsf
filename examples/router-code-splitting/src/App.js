e4b6f5c8d1f08bb4aac253986dd9a8754a787520
xianshannan <xianshannan@qq.com>
2018-12-07 11:56:53 +0800

    add router-code-spltting and remove react@15.xxx example

--- examples/router-code-splitting/src/App.js
+++ examples/router-code-splitting/src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'rsf-router';
 // to reduce code, you can use the following
 // import { default as Router, Route } from 'rsf-router/lib/browser-router';
 
 import MainLayout from './view/layout/main';
-import Homeiew from './view/main/home';
-import AboutView from './view/main/about';
-import TopicsView from './view/main/topics';
-import NotFound from './view/not-found';
+
+const HomeView = React.lazy(() => import('./view/main/home'));
+const AboutView = React.lazy(() => import('./view/main/about'));
+const TopicsView = React.lazy(() => import('./view/main/topics'));
+const NotFound = React.lazy(() => import('./view/not-found'));
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
         <MainLayout>
-          <Route path="/main/home" component={Homeiew} index />
-          <Route path="/main/about" component={AboutView} />
-          <Route path="/main/topics" component={TopicsView} />
-          <Route component={NotFound} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Route path="/main/home" component={HomeView} index />
+            <Route path="/main/about" component={AboutView} />
+            <Route path="/main/topics" component={TopicsView} />
+            <Route component={NotFound} />
+          </Suspense>
         </MainLayout>
       </Router>
     );
