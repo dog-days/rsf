42e618dac3c28f823addb59ded1e42d4c926d9ff
xianshannan <xianshannan@qq.com>
2018-12-11 15:12:22 +0800

    update packages/rsf rollup.config.js

--- packages/rsf/rollup.config.js
+++ packages/rsf/rollup.config.js
@@ -1,36 +1,31 @@
-// nodeResolve是必须的，要不watch模式会报错。
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-// import replace from 'rollup-plugin-replace';
-// import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
 //babel需要再 commonjs plugin 之前配置
 const commonjsPlugin = commonjs({
-  // include: /node_modules/,
-  // include: [
-  //   /node_modules\/prop-types/,
-  //   /node_modules\/hoist-non-react-statics/,
-  //   /node_modules\/invariant/,
-  //   /node_modules\/react-is/,
-  //   /node_modules\/warning/,
-  // ],
-  // namedExports: {
-  //   'node_modules/react-js/index.js': ['isValidElementType'],
-  // },
+  include: /node_modules/,
 });
 
-function createCommonConfigByInput(input, fileName, umdName) {
+function createCommonConfigByInput(input, fileName) {
   return [
     // CommonJS
     {
       input,
-      output: { file: `lib/${fileName}.js`, format: 'cjs', indent: false },
+      output: {
+        file: `lib/${fileName}.js`,
+        format: 'cjs',
+        indent: false,
+        // 消除 bundle['default'] to access the default export
+        exports: 'named',
+      },
       external: [
         ...Object.keys(pkg.dependencies || {}),
         ...Object.keys(pkg.peerDependencies || {}),
+        'lodash/merge',
+        'lodash/cloneDeep',
       ],
       plugins: [
         nodeResolve(),
@@ -47,6 +42,8 @@ function createCommonConfigByInput(input, fileName, umdName) {
       external: [
         ...Object.keys(pkg.dependencies || {}),
         ...Object.keys(pkg.peerDependencies || {}),
+        'lodash/merge',
+        'lodash/cloneDeep',
       ],
       plugins: [
         nodeResolve(),
@@ -54,73 +51,7 @@ function createCommonConfigByInput(input, fileName, umdName) {
         commonjsPlugin,
       ],
     },
-
-    // // UMD Development
-    // {
-    //   input,
-    //   output: {
-    //     file: `dist/${fileName}.js`,
-    //     format: 'umd',
-    //     name: umdName,
-    //     indent: false,
-    //     sourcemap: false,
-    //   },
-
-    //   external: ['react', 'react-dom'],
-    //   globals: {
-    //     React: 'React',
-    //     ReactDOM: 'ReactDOM',
-    //   },
-    //   plugins: [
-    //     // replace({
-    //     //   'process.env.NODE_ENV': JSON.stringify('development'),
-    //     // }),
-    //     babel({
-    //       exclude: 'node_modules/**',
-    //     }),
-    //     nodeResolve({
-    //       jsnext: true,
-    //       main: true,
-    //     }),
-    //     commonjsPlugin,
-    //   ],
-    // },
-
-    // // UMD Production
-    // {
-    //   input,
-    //   output: {
-    //     file: `dist/${fileName}.min.js`,
-    //     format: 'umd',
-    //     name: umdName,
-    //     indent: false,
-    //     sourcemap: false,
-    //   },
-    //   external: ['react', 'react-dom'],
-    //   globals: {
-    //     React: 'React',
-    //     ReactDOM: 'ReactDOM',
-    //   },
-    //   plugins: [
-    //     nodeResolve(),
-    //     replace({
-    //       'process.env.NODE_ENV': JSON.stringify('production'),
-    //     }),
-    //     babel({
-    //       exclude: 'node_modules/**',
-    //     }),
-    //     commonjsPlugin,
-    //     terser({
-    //       compress: {
-    //         pure_getters: true,
-    //         unsafe: true,
-    //         unsafe_comps: true,
-    //         warnings: false,
-    //       },
-    //     }),
-    //   ],
-    // },
   ];
 }
 
-export default [...createCommonConfigByInput('src/index.js', 'rsf', 'Rsf')];
+export default [...createCommonConfigByInput('src/index.js', 'rsf')];
